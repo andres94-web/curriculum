6e6029634525948a5f3e7a4d412b38daa7541a0b
andres94-web <andresvallelap@gmail.com>
2021-12-25 15:43:28 -0500

    primera prueba

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -6,7 +6,7 @@ const contenedor = () => {
             <h1 className='text-center display-6'>Alvaro Andres Hurtado Vallecilla</h1>
             <p className='text-center m-0'>Estudiante de ingenieria en sistemas</p>
             <p className='text-center'>Universidad del Valle</p>
-            
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem voluptates quas vitae eum perspiciatis aliquam natus id voluptatem! Non harum veniam, architecto nulla blanditiis atque officia placeat ipsa unde aliquam.
         </div>
     )
 }
